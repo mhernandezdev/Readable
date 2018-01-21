5e1026b26cafa331cbd236808f3c339e7625a90a
Mike Hernandez <bad.fish.mh@gmail.com>
2018-01-21 08:16:26 -0500

    Added 2 files

---
+++ src/components/Post.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import { capitalize } from '../utils/helpers'
+import { connect } from 'react-redux'
+import { withRouter, Link } from 'react-router-dom'
+import Vote from './Vote'
+import Reply from './Reply'
+import TiUser from 'react-icons/lib/ti/user'
+import MdMoreVert from 'react-icons/lib/md/more-vert'
+
+const Post = (props) => {
+
+    const { post, category, id, comments, detail } = props;
+
+    // link based on URL // direct to post or comment
+    let linkTo = `/category/${category}/post/${post.id}`;
+
+    // if an id on the url then we are already on a post details page - opening a child comment
+    if(id && post && post.parentId){
+        linkTo = `/category/${category}/post/${post.parentId}/${post.id}`;
+    }
+
+    // get comments // list newest to oldest
+    let myComments = [];
+    comments && (myComments = Object.values(comments)
+                                .filter(comment => comment.parentId===post.id))
+                                .sort((a, b) => b.timestamp > a.timestamp);
+
+    return (
+        <div className={`post ${(post.parentId && 'post-comment') || 'post-root'}`}>
+
+            <Link to={linkTo} className={`post-link ${detail ? 'post-link-disabled' : ''}`}>
+                <div className="post-user-icon">
+                    <TiUser size={20} />
+                </div>
+
+                <div className="post-wrap-author-date">
+                    <span className="post-author">{post.author}</span>
+                    <span className="post-date">
+                    &nbsp;on { new Date(post.timestamp).toDateString() } { new Date(post.timestamp).toLocaleTimeString() }
+                    </span>
+
+                    <div className="post-more-icon"><MdMoreVert size={20} /></div>
+                </div>
+
+                { post.category &&
+                    <span>
+                        <span className="post-category">{capitalize(post.category)}:</span> <span className="post-title">{post.title}</span>
+                    </span>
+                }
+
+                <div className="post-body">{post.body}</div>
+            </Link>
+
+            <Vote post={post} />
+
+            {!post.parentId &&
+                <Reply comments={ myComments.length>0 } parentId={post.id}  />
+            }
+
+            {myComments.map(comment => (
+                <Post key={comment.id} post={comment} category={category} id={id}></Post>
+            ))}
+
+        </div>
+    )
+}
+
+function mapStateToProps ({ comments }) {
+    return { comments };
+}
+
+export default withRouter(connect(
+    mapStateToProps
+)(Post))
---
+++ src/components/PostForm.js
@@ -0,0 +1,139 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { addPost, updatePost } from '../actions'
+
+class PostForm extends Component {
+    static defaultProps = {
+        /* post - seed data for internally-controlled state */
+        post: {
+            category:'react',
+            author:'',
+            title:'',
+            body:'',
+            id: Math.random().toString(36).substr(-8),
+            parentId:''
+        },
+        type:'comments',
+        mode:'edit'
+    }
+
+    state = {
+        category:this.props.post.category,
+        author:this.props.post.author,
+        title:this.props.post.title,
+        body:this.props.post.body,
+        authorFail:false,
+        titleFail:false,
+        bodyFail:false
+    }
+
+    componentDidMount(){
+        console.log(this.props)
+    }
+    componentWillReceiveProps(nextProps){
+        console.log('nextProps', nextProps)
+    }
+
+    categoriesChange= (e) => {
+        this.setState({ category: e.target.value });
+    }
+
+    handleTextChange = (e) => {
+        const {name, value} = e.target;
+        this.setState({ [name]: value, [`${name}Fail`]:value==='' });
+    }
+
+    handlePost = (e) => {
+        e.preventDefault();
+
+        const { type, mode, post } = this.props;
+        const { id, parentId } = post;
+
+        // some validation
+        const { category, author, title, body } = this.state;
+        this.setState({ authorFail:author==='', titleFail:title==='', bodyFail:body==='' });
+
+        const { authorFail, titleFail, bodyFail } = this.state;
+        if(!authorFail && !bodyFail && (type!=='posts' || !titleFail)){
+            // format body info
+            const data = {
+                category,
+                parentId,
+                id,
+                author,
+                title,
+                body,
+                timestamp: Date.now(),
+            }
+            //console.log('mode', mode, 'data',data)
+
+            this.props[(mode==='edit' && 'updatePost') || 'addPost']({ type:type, body:data, id:id });
+
+            this.props.edit();
+        }
+    }
+
+    render() {
+        const { type, categories } = this.props;
+        const { category, author, title, body, authorFail, titleFail, bodyFail } = this.state;
+
+        return (
+            <div className="post-add-comment">
+
+                    <form onSubmit={this.handlePost}>
+
+                        {type==='posts' && <select className="" value={category} onChange={(e) => this.categoriesChange(e)}>
+                            {categories.map(c => (c.name!=='all' &&
+                                <option key={c.name} value={c.name}>{c.name}</option>
+                            ))}
+                        </select>}
+
+                        <input
+                        type="text"
+                        autoFocus placeholder="Author"
+                        name="author"
+                        value={ author }
+                        onChange={ this.handleTextChange }
+                        className={ authorFail ? 'FAIL' : '' }
+                        />
+
+                        {type==='posts' && <input
+                        type="text"
+                        placeholder="Title"
+                        name="title"
+                        value={ title }
+                        onChange={ this.handleTextChange }
+                        className={ titleFail ? 'FAIL' : '' }
+                        />
+                        }
+
+                        <textarea
+                        placeholder="Comment"
+                        name="body"
+                        value={ body }
+                        onChange={ this.handleTextChange }
+                        className={ bodyFail ? 'FAIL' : '' }
+                        />
+
+
+                        <div className="button" onClick={() => this.props.edit()}>Cancel</div>
+                        <button type="submit">Post</button>
+                    </form>
+
+            </div>
+        )
+    }
+}
+
+function mapStateToProps({ categories }) {
+    return { categories }
+}
+function mapDispatchToProps (dispatch) {
+    return {
+        addPost: (data) => dispatch(addPost(data)),
+        updatePost: (data) => dispatch(updatePost(data))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostForm);
+
